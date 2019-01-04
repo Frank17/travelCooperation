ea2ae11001b6d8b71511d32c2b32bc43d21228a2
Frank Tsau <frank@FrankdeMacBook-Pro-2.local>
2019-01-04 18:10:20 +0800

    添加输入弹窗

--- pages/publication/publication.js
+++ pages/publication/publication.js
@@ -1,18 +1,33 @@
 // pages/publication/publication.js
+import moment from '../../miniprogram_npm/moment/index.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    maxMemberPickerShown: false,
+    maxMemebersValues: [],
+    maxMember: 10,
+    startDatePickerShown: false,
+    endDatePickerShown: false,
+    startDate: moment().format('YYYY/MM/DD HH:mm'),
+    startDateValue: moment().millisecond(),
+    endDate: moment().add(12, 'h').format('YYYY/MM/DD HH:mm'),
+    endDateValue: moment().add(12, 'h').millisecond(),
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let values = []
+    for (let i = 1; i <= 140; i++) {
+      values.push(i);
+    }
+    this.setData({
+      maxMemebersValues: values
+    })
   },
 
   /**
@@ -62,5 +77,72 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  showPicker(event) {
+    let { currentTarget } = event
+    let pickerType = currentTarget.dataset.pickerType
+    if (pickerType === 'maxMember') {
+      this.setData({
+        maxMemberPickerShown: true,
+      })
+    } else if (pickerType === 'startDatePicker') {
+      this.setData({
+        startDatePickerShown: true,
+      })
+    } else if (pickerType === 'endDatePicker') {
+      this.setData({
+        endDatePickerShown: true,
+      })
+    } else if (pickerType === 'addressPicker') {
+
+    }
+  },
+  onClosePicker(event) {
+    let { currentTarget } = event
+    let pickerType = currentTarget.dataset.forPicker
+    if (pickerType === 'maxMember') {
+      this.setData({
+        maxMemberPickerShown: false,
+      })
+    } else if (pickerType === 'startDatePicker') {
+      this.setData({
+        startDatePickerShown: false,
+      })
+    } else if (pickerType === 'endDatePicker') {
+      this.setData({
+        endDatePickerShown: false,
+      })
+    } else if (pickerType === 'addressPicker') {
+
+    }
+  },
+  onCloseMaxMemberPicker() {
+    this.setData({
+      maxMemberPickerShown: false,
+    })
+  },
+  onMaxMemberChange() {
+
+  },
+  onMaxMemberConfirm(event) {
+    let { detail } = event
+    console.log(detail)
+    this.setData({
+      maxMember: detail.value,
+      maxMemberPickerShown: false,
+    })
+  },
+  onMaxMemberCancel() {
+    this.setData({
+      maxMemberPickerShown: false
+    })
+  },
+  onCloseStartDatePicker() {
+    this.setData({
+      startDatePickerShown: false,
+    })
+  },
+  nowDate() {
+    return new Date().getTime()
+  },
 })
