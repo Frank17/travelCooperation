2e6e9d7cec2d9ac7b7d5789eb0414eb2b54cf4c6
Frank Tsau <frank@FrankdeMacBook-Pro-2.local>
2019-01-10 11:34:25 +0800

    更新登录接口

--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,13 @@
+let __DEBUG__ = true
+
+const getServer = () => {
+  if (__DEBUG__) {
+    return 'http://'
+  } else {
+    return 'http://'
+  }
+}
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -14,6 +24,76 @@ const formatNumber = n => {
   return n[1] ? n : '0' + n
 }
 
+const post = (url, param) => {
+  return new Promise((resolve, reject) => {
+      wx.request({
+      url: url,
+      data: param,
+      method: 'POST',
+      header: 'application/json;charset=utf-8',
+      success(res) {
+        resolve(res)
+      },
+      fail(err) {
+        reject(res)
+      } 
+    })
+  })
+}
+
+const getRequest = (url, param) => {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: url,
+      data: param,
+      method: 'GET',
+      success(res) {
+        resolve(res)
+      },
+      fail(err) {
+        reject(res)
+      }
+    })
+  })
+}
+
+const login = () => {
+  let loginPromise = new Promise((resolve, reject) => {
+    wx.checkSession({
+      success() {
+        resolve(true)
+      },
+      fail() {
+        reject(false)
+      }
+    })
+  })
+  loginPromise.then(status => {
+    console.log('logined')
+  })
+  .catch(status => {
+    wx.login({
+      success(res) {
+        console.log(res)
+        let { code } = res
+        post(`${getServer()}/user/login/${code}`)
+        .then(loginResult => {
+
+        })
+        .catch(err => {
+
+        })
+      },
+      fail(err) {
+        console.log(err)
+      }
+    })
+  })
+}
+
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  login: login,
+  post: post,
+  getRequest: getRequest
 }
